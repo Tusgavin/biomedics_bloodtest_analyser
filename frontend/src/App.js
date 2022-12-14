a198dede97a1a969bae49a9e2e72a75bdc68feb3
maTh51 <matheustigon@gmail.com>
2022-12-14 00:07:17 -0300

    Changed table columns and added charts for each exam

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,43 @@
-import React, { useState } from 'react';
-import { LineChartOutlined, TableOutlined } from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
+import React, { useState } from "react";
+import { LineChartOutlined, TableOutlined } from "@ant-design/icons";
+import { Layout, Menu } from "antd";
 
-import { Chartview } from './components/Chart';
-import { Tableview } from './components/Table';
+import { Chartview } from "./components/Chart";
+import { Tableview } from "./components/Table";
 
-import './App.css'
+import "./App.css";
 
 const { Content, Sider } = Layout;
 
-const menu = [{
-  icon: LineChartOutlined,
-  name: "Chart"
-}, {
-  icon: TableOutlined,
-  name: "Table"
-}]
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+
+const menu = [
+  getItem("Charts", "1", LineChartOutlined, [
+    getItem("Colesterol HDL", "sub1"),
+    getItem("Colesterol LDL", "sub2"),
+    getItem("Colesterol não-LDL", "sub3"),
+    getItem("Colesterol Total", "sub4"),
+    getItem("Colesterol VLDL", "sub5"),
+    getItem("Creatinina", "sub6"),
+    getItem("Glicemia", "sub7"),
+    getItem("Hemacias", "sub8"),
+    getItem("Hemoglobina", "sub9"),
+    getItem("Índice de Saturacao da Transferrina", "sub10"),
+    getItem("Leucocitos", "sub11"),
+    getItem("T4 Livre", "sub12"),
+    getItem("Triglicerides", "sub13"),
+    getItem("TSH Ultra Sensível", "sub14"),
+  ]),
+  getItem("Table", "2", TableOutlined),
+];
 
 const App = () => {
   const [active, setActive] = useState(0);
@@ -26,27 +48,25 @@ const App = () => {
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={['0']}
-          items={menu.map(
-            (item, index) => ({
-              key: String(index),
-              icon: React.createElement(item.icon),
-              label: `${item.name}`
-            }),
-          )}
-          onClick={({key}) => setActive(key)}
+          defaultSelectedKeys={["0"]}
+          items={menu.map((item, index) => ({
+            key: String(index),
+            icon: React.createElement(item.icon),
+            label: `${item.label}`,
+            children: item.children,
+          }))}
+          onClick={({ key }) => setActive(key)}
         />
       </Sider>
       <Layout>
-        <Content style={{ margin: '60px 60px' }}>
-          <div style={{ height: '100%' }}>
-            { active === '0' && <Chartview />}
-            { active === '1' && <Tableview />}
-            </div>
+        <Content style={{ margin: "60px 60px" }}>
+          <div style={{ height: "100%" }}>
+            {active !== "1" ? <Chartview /> : <Tableview />}
+          </div>
         </Content>
       </Layout>
     </Layout>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Table/mock/mock.js
+++ frontend/src/components/Table/mock/mock.js
@@ -1,35 +1,86 @@
 export const data = Array(100).fill({
-   date: '21/01/2022',
-   plaquetas: '170000',
-   leucocitos: '4490',
-   hemacias: '5,07',
-   vcm: '48',
-   hcm: '95'
+  cpf: "38025835073",
+  dataDaConsulta: "1670900400000",
+  colesterolHDL: "50",
+  colesterolLDL: "50",
+  colesterolNaoLDL: "50",
+  colesterolTotal: "50",
+  colesterolVLDL: "50",
+  creatinina: "50",
+  glicemia: "50",
+  hemacias: "50",
+  hemoglobina: "50",
+  indiceDeSaturacaoDaTransferrina: "50",
+  leucocitos: "50",
+  plaquetas: "50",
+  tQuatroLivre: "50",
+  triglicerides: "50",
+  tshUltraSensivel: "50",
 });
 
 export const columns = [
-   {
-      title: 'Data',
-      dataIndex: 'date'
-   },
-   {
-      title: 'Plaquetas',
-      dataIndex: 'plaquetas'
-   },
-   {
-      title: 'Leucócitos',
-      dataIndex: 'leucocitos'
-   },
-   {
-      title: 'Hemácias',
-      dataIndex: 'hemacias'
-   },
-   {
-      title: 'V.C.M.',
-      dataIndex: 'vcm'
-   },
-   {
-      title: 'H.C.M.',
-      dataIndex: 'hcm'
-   },
+  {
+    title: "CPF",
+    dataIndex: "cpf",
+  },
+  {
+    title: "Data da Consulta",
+    dataIndex: "dataDaConsulta",
+  },
+  {
+    title: "Colesterol HDL",
+    dataIndex: "colesterolHDL",
+  },
+  {
+    title: "Colesterol LDL",
+    dataIndex: "colesterolLDL",
+  },
+  {
+    title: "Colesterol não-LDL",
+    dataIndex: "colesterolNaoLDL",
+  },
+  {
+    title: "Colesterol Total",
+    dataIndex: "colesterolTotal",
+  },
+  {
+    title: "Creatinina",
+    dataIndex: "creatinina",
+  },
+  {
+    title: "Glicemia",
+    dataIndex: "glicemia",
+  },
+  {
+    title: "Hemácias",
+    dataIndex: "hemacias",
+  },
+  {
+    title: "Hemoglobina",
+    dataIndex: "hemoglobina",
+  },
+  {
+    title: "Índice de Saturação da Transferrina",
+    dataIndex: "indiceDeSaturacaoDaTransferrina",
+  },
+  {
+    title: "Leucócitos",
+    dataIndex: "leucocitos",
+  },
+  {
+    title: "Plaquetas",
+    dataIndex: "plaquetas",
+  },
+  {
+    title: "T4 Livre",
+    dataIndex: "tQuatroLivre",
+  },
+  {
+    title: "Triglicerides",
+    dataIndex: "triglicerides",
+  },
+  {
+    title: "TSH Ultra Sensível",
+    dataIndex: "tshUltraSensivel",
+  },
 ];
