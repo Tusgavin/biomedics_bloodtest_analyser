55cfe00beb8378407dadf58b854fa73c43078c40
maTh51 <matheustigon@gmail.com>
2022-12-14 00:08:22 -0300

    Merge branch 'master' of https://github.com/Tusgavin/biomedics_bloodtest_analyser

---
+++ backend/Exam.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const ExamSchema = new mongoose.Schema({
+   tshUltraSensivel: {
+      type: Number
+   },
+   triglicerides: {
+      type: Number
+   },
+   tQuatroLivre: {
+      type: Number
+   },
+   plaquetas: {
+      type: Number
+   },
+   leucocitos: {
+      type: Number
+   },
+   creatinina: {
+      type: Number
+   },
+   indiceDeSaturacaoDaTransferrina: {
+      type: Number
+   },
+   hemoglobina: {
+      type: Number
+   },
+   hemacias: {
+      type: Number
+   },
+   glicemia: {
+      type: Number
+   },
+   colesterolVLDL: {
+      type: Number
+   },
+   colesterolTotal: {
+      type: Number
+   },
+   colesterolNaoLDL: {
+      type: Number
+   },
+   colesterolLDL: {
+      type: Number
+   },
+   colesterolHDL: {
+      type: Number
+   },
+   cpf: {
+      type: String,
+      required: true
+   },
+   dataDaConsulta: {
+      type: String
+   }
+});
+
+const Exam = mongoose.model('Exam', ExamSchema);
+
+module.exports = Exam;
